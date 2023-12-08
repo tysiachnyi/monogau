cab03875726c3e21ecc2da1e82fd5c0bacf278c2
Nik <nikita.tysiachnyi@gmail.com>
2023-12-08 12:28:00 +0100

    Example of VSCode extension for WORKSHOP

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,56 @@
+
 import * as vscode from 'vscode';
+import { exec } from 'child_process';
+import {  commandList, getWebviewContentCommands } from './commands';
 
 
-function createWebviewPanel(title: string, content: string) {
-	const panel = vscode.window.createWebviewPanel(
-			'extensionUI',
-			title,
-			vscode.ViewColumn.One,
-			{}
-	);
 
-	panel.webview.html = content;
+function createWebviewPanel(context: vscode.ExtensionContext, title: string, content: string) {
+    const panel = vscode.window.createWebviewPanel(
+        'extensionUI',
+        title,
+        vscode.ViewColumn.One,
+        {
+            enableScripts: true
+        }
+    );
+
+    panel.webview.onDidReceiveMessage(
+        message => {
+            if (message.command === 'runCommand') {
+                
+                exec(message.text, (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(`exec error: ${error}`);
+                        return;
+                    }
+                    console.log(`stdout: ${stdout}`);
+                    console.error(`stderr: ${stderr}`);
+                });
+
+                vscode.window.showInformationMessage(`Running command: ${message.text}`);
+            }
+        },
+        undefined,
+        context.subscriptions
+    );
+
+    panel.webview.html = content;
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	let openCommandsDisposable = vscode.commands.registerCommand('monogau.openCommands', () => {
-		createWebviewPanel('Commands', getWebviewContentCommands());
-});
+    let openCommandsDisposable = vscode.commands.registerCommand('monogau.openCommands', () => {
+        createWebviewPanel(context, 'Commands', getWebviewContentCommands(commandList));
+    });
 
-let openDocumentationDisposable = vscode.commands.registerCommand('monogau.openDocumentation', () => {
-		createWebviewPanel('Documentation', getWebviewContentDocumentation());
-});
+    let openDocumentationDisposable = vscode.commands.registerCommand('monogau.openDocumentation', () => {
+        createWebviewPanel(context, 'Documentation', getWebviewContentDocumentation());
+    });
 
-context.subscriptions.push(openCommandsDisposable, openDocumentationDisposable);
+    context.subscriptions.push(openCommandsDisposable, openDocumentationDisposable);
 
     const treeDataProvider = new MyTreeDataProvider();
     vscode.window.createTreeView('myView', { treeDataProvider });
-
 }
 
 class MyTreeDataProvider implements vscode.TreeDataProvider<string> {
@@ -55,14 +80,9 @@ class MyTreeDataProvider implements vscode.TreeDataProvider<string> {
     }
 }
 
-function getWebviewContentCommands() {
-    return `
-        <html>
-        <body>
-            <h1>Hello, World! Commands </h1>
-        </body>
-        </html>`;
-}
+
+
+
 
 function getWebviewContentDocumentation() {
 	return `
